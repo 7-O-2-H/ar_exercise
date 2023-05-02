fa4e0cdff785de0df8c27bb5b8b21d3d75a1588f
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-05-02 15:08:32 -0400

    added exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "Total annual revenue: #{total_revenue}."
+
+average_revenue = Store.average(:annual_revenue).round()
+puts "Average annual revenue: #{average_revenue}"
+
+revenue_over_one_million = Store.where(annual_revenue: 100000..Float::INFINITY).count
+puts "There are #{revenue_over_one_million} stores generating at least $1,000,000"
