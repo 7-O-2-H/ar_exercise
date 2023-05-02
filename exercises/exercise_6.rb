c2cb87c1e181eea3aaafa64072d35df096409744
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-05-02 15:30:55 -0400

    added exercises 6 and 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,10 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 20)
+@store1.employees.create(first_name: "Josh", last_name: "Kelly", hourly_rate: 25)
+@store1.employees.create(first_name: "Jack", last_name: "Black", hourly_rate: 30)
+
+@store2.employees.create(first_name: "Edward", last_name: "Scissorhands", hourly_rate: 20)
+@store2.employees.create(first_name: "Scott", last_name: "Jones", hourly_rate: 32)
+@store2.employees.create(first_name: "Jake", last_name: "Samson", hourly_rate: 35)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,16 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Please enter a store name:"
+store_name = gets.chomp
+
+new_store = Store.create(name: store_name, annual_revenue: 1250000, mens_apparel: true, womens_apparel: false)
+
+if new_store.errors.any?
+  puts "Errors occurred while saving the store:"
+  new_store.errors.full_messages.each do |message|
+    puts message
+  end
+else
+  puts "Store was saved successfully!"
+end
